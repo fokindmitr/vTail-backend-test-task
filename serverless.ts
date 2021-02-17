56faa9fad39fc53499b7ae458228fc9fd3dbc38b
Dmitry Fokin <dmitry.fokin@softermii.com>
2021-02-17 11:34:26 +0200

    serverless initial app setup

---
+++ serverless.ts
@@ -0,0 +1,36 @@
+import type { AWS } from '@serverless/typescript';
+
+import { hello } from './src/functions';
+
+const serverlessConfiguration: AWS = {
+  service: 'aws-nodejs-typescript',
+  frameworkVersion: '2',
+  custom: {
+    webpack: {
+      webpackConfig: './webpack.config.js',
+      includeModules: true
+    },
+    'serverless-offline': {
+      httpPort: 4000,
+      stageVariables: {
+        foo: "bar"
+      }
+    }
+  },
+  plugins: ['serverless-webpack', 'serverless-offline'],
+  provider: {
+    name: 'aws',
+    runtime: 'nodejs12.x',
+    apiGateway: {
+      minimumCompressionSize: 1024,
+      shouldStartNameWithService: true,
+    },
+    environment: {
+      AWS_NODEJS_CONNECTION_REUSE_ENABLED: '1',
+    },
+    lambdaHashingVersion: '20201221',
+  },
+  functions: { hello }
+}
+
+module.exports = serverlessConfiguration;
